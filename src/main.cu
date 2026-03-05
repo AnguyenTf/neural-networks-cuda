c3fed1957ceb645094a32330b1840320cfbc8e82
anguyenTf <anguyen57329@gmail.com>
2026-03-05 10:04:06 -0800

    testing

--- src/main.cu
+++ src/main.cu
@@ -10,12 +10,20 @@
 #####                KERNELS                    #####
 ###################################################*/
 
-__global__ void linear_layer_and_activation(float *weight_matrix, float *biases, float *x_inputs,
-                 float *z_values, float *activation_values,
-                 int nr_output_neurons, int nr_input_neurons){
-    
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    
+__global__ void matMul(float* A, float* B, float* C, 
+                            int M, int K, int N) {
+
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < M && col < N){
+        float sum = 0.0f;
+        
+        for (int i = 0; i < K; i++) {
+            sum += A[row * K + i] * B[i * N + col];
+        }
+    C[row * N + col] = sum;
+    }
 
 }
 
@@ -25,7 +33,51 @@ __global__ void linear_layer_and_activation(float *weight_matrix, float *biases,
 
 int main() {
     
-    std::cout << "Hellow bruh" << std::endl;
+    int M = 3, K = 2, N = 4;
+
+    float h_A[M*K] = {
+        1, 2,
+        3, 4,
+        5, 6
+    };
+
+    float h_B[K*N] = {
+        7, 8, 9, 10,
+        11, 12, 13, 14
+    };
+
+    float h_C[M*N];
+
+    float *d_A, *d_B, *d_C;
+
+    cudaMalloc(&d_A, M*K*sizeof(float));
+    cudaMalloc(&d_B, K*N*sizeof(float));
+    cudaMalloc(&d_C, M*N*sizeof(float));
+    
+    cudaMemcpy(d_A, h_A, M*K*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, K*N*sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 threadsPerBlock(16,16);
+    dim3 blocksPerGrid((N + 16)/16, (M+15)/16);
+
+    matMul<<<blocksPerGrid, threadsPerBlock>>>(d_A,d_B, d_C, M, K, N);
+
+    cudaMemcpy(h_C, d_C, M*N*sizeof(float), cudaMemcpyDeviceToHost);
+
+    std::cout << "Result matrix C:\n";
+
+    for (int i = 0; i < M; i++){
+        for (int j = 0; j < N; j++){
+            std::cout << h_C[i*N + j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
 
     return 0;
+    
 }
