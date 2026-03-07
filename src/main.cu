1c4889ca77d434f1c207da8abf4e7733260d77aa
Andy Nguyen <anguyen57329@gmail.com>
2026-03-07 15:21:48 -0800

    Refactor to use GPU for array addition and remove matMul
    Removed matrix multiplication kernel and related code. Updated add function to run on GPU and allocated memory using cudaMallocManaged.

--- src/main.cu
+++ src/main.cu
@@ -10,31 +10,14 @@
 #####                KERNELS                    #####
 ###################################################*/
 
-<<<<<<< HEAD
-__global__ void matMul(float* A, float* B, float* C, 
-                            int M, int K, int N) {
-
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (row < M && col < N){
-        float sum = 0.0f;
-        
-        for (int i = 0; i < K; i++) {
-            sum += A[row * K + i] * B[i * N + col];
-        }
-    C[row * N + col] = sum;
-    }
-
-=======
 // Function to add the elements of two arrays
 void add(int n, float *x, float *y){
-    // 
+    int index = threadIdx.x;
+    int stride = blockDim.x
+
     for (int i = 0; i < n; i++){
         y[i] = x[i] + y[i];
     }
->>>>>>> 31fdb8e (testing adding the element of two arrays on the CPU)
-}
 
 /*##################################################
 #####                MAIN                      #####
@@ -45,9 +28,11 @@ int main(void){
     // N = 00000000 00001000 00000000 00000000
     int N = 1<<20;
 
-    // Create 
-    float *x = new float[N];
-    float *y = new float[N];
+    float *x, *y;
+
+    // Allocate Unified Memory - accessible from CPU or GPU
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
 
     // Initialize x and y arrays on the host
     for (int i = 0; i < N; i++) {
@@ -56,69 +41,16 @@ int main(void){
     }
 
     // Run kernel on 1M elemnets on the CPU
-    add(N, x, y);
+    add<<<1,256>>>(N, x, y);
 
     // Check for errors (all values should be 3.0f)
     float maxError = 0.0f;
     for (int i = 0; i < N; i++)
     maxError = fmax(maxError, fabs(y[i] - 3.0f));
     std::cout << "Max error: " << maxError << std::endl;
-    
-<<<<<<< HEAD
-    int M = 3, K = 2, N = 4;
 
-    float h_A[M*K] = {
-        1, 2,
-        3, 4,
-        5, 6
-    };
-
-    float h_B[K*N] = {
-        7, 8, 9, 10,
-        11, 12, 13, 14
-    };
-
-    float h_C[M*N];
-
-    float *d_A, *d_B, *d_C;
-
-    cudaMalloc(&d_A, M*K*sizeof(float));
-    cudaMalloc(&d_B, K*N*sizeof(float));
-    cudaMalloc(&d_C, M*N*sizeof(float));
-    
-    cudaMemcpy(d_A, h_A, M*K*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, K*N*sizeof(float), cudaMemcpyHostToDevice);
-
-    dim3 threadsPerBlock(16,16);
-    dim3 blocksPerGrid((N + 16)/16, (M+15)/16);
-
-    matMul<<<blocksPerGrid, threadsPerBlock>>>(d_A,d_B, d_C, M, K, N);
-
-    cudaMemcpy(h_C, d_C, M*N*sizeof(float), cudaMemcpyDeviceToHost);
-
-    std::cout << "Result matrix C:\n";
-
-    for (int i = 0; i < M; i++){
-        for (int j = 0; j < N; j++){
-            std::cout << h_C[i*N + j] << " ";
-        }
-        std::cout << "\n";
-    }
-
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
-
+    cudaFree(x);
+    cudaFree(y);
 
     return 0;
-    
-=======
-
-    // Free memory
-    delete [] x;
-    delete [] y;
-
-    return 0;
-
->>>>>>> 31fdb8e (testing adding the element of two arrays on the CPU)
 }
