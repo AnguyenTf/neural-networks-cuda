bf6eaf4818b7ecf722b1e1bb676ac59f5eaab5c5
AnguyenTf <anguyen57329@gmail.com>
2026-03-11 19:20:34 -0700

    Populated dot.cuh with the essientials to matrix multply operations

--- include/matrix_ops.cuh
+++ include/matrix_ops.cuh
@@ -5,10 +5,10 @@
 Header declares the kernels and device wrappers
 */
 
-//
+// Elementwise subtraction: out[i] = a[i] - b[i]
 __global__ void kSub(const float *a, const float *b, float *out, int n);
 __device__ void dSub(const float *a, const float *b, float *out, int rows, int cols);
 
-//
+// Elementwise multiplication: out[i] = a[i] * b[i]
 __global__ void kMul(const float *a, const float *b, float *out, int n);
 __global__ void dMul(const float *a, const float *b, float *out, int rows, int cols);
--- src/matrix_ops.cu
+++ src/matrix_ops.cu
@@ -1,7 +1,53 @@
 #include "matrix_ops.cuh"
 
+
+/*
+* Elementwise subtraction
+* Used in backprop to compute
+*/
 __global__ void kSub(const float *a, const float *b, float *out, int n){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
 
+    for (; i < n; i += blockDim.x * gridDim.x){
+        out[i] = a[i] - b[i];
+    }
+}
+
+/*
+* Device wrapper for kSub - refer to matrix_ops.cuh
+* Allows subtraction to be called from inside other GPU kernels
+*/
+__device__ void dSub(const float *a, const float *b, float *out, int rows, int cols){
+    int n = rows * cols;
+    int threads = 256;
+    int blocks = (n + threads - 1) / threads;
+
+    kSub<<<blocks, threads>>>(a, b, out, n);
+    cudaDeviceSynchronize();
+}
+
+/*
+* Elementwise Multiplication - refer to matrix_ops.cuh
+* Used in backprop to compute: delta = error * sigmoid_derivative
+*/
+__global__ void kMul(const float *a, const float *b, float *out, int rows, int n){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    for(; i < n; i += blockDim.x * gridDim.x){
+        out[i] = a[i] + b[i];
+    }
     
+}
+
+/*
+* Device wrapper for kMul - refer to matrix_ops.cuh
+* Allow multiplication to be called from inside other GPU kernels
+*/
+__device__ void dMUl(const float *a, const float *b, float *out, int rows, int cols){
+    int n = rows * cols;
+    int threads = 256; 
+    int blocks = (n + threads - 1) / threads;
+
+    kMul<<<blocks, threads>>>(a, b, out, n);
+    cudaDeviceSynchronize();
 }
